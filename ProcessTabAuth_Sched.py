fa1f198c538d29598a2406459dc4b7027c582cb4
Mok3bat <thewiseeng@gmail.com>
2023-07-18 16:00:35 +0300

    Scheduler Code

---
+++ ProcessTabAuth_Sched.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+import subprocess
+
+def run_script():
+    subprocess.call(["python3", "/path/to/mok3bat_wsgi.py"])
+
+# Schedule the script to run every 8 minutes
+schedule.every(9).minutes.do(run_script)
+
+# Keep the script running continuously
+while True:
+    schedule.run_pending()
+    time.sleep(9*60+3)
