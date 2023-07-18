d6be9d21238457b3bddefd646dfb48909a0e2cb7
Mok3bat <thewiseeng@gmail.com>
2023-07-18 15:22:39 +0300

    Modify Code

--- ProcessTabAuth.py
+++ ProcessTabAuth.py
@@ -3,9 +3,12 @@ import datetime
 import uuid
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import git
-#import schedule
-#import time
+
+import schedule
+import time
+
+# Define a global variable for the token
+token = None
 
 #prerequisite values
 connectedAppClientId = '0a9faf71-d52e-4a17-a52a-24a0d5ad694c'
@@ -18,7 +21,11 @@ username = "m.setit@gmail.com"
 #TOL or Tableau server name. SSL is highly recommeded
 tableauservername = "https://10ax.online.tableau.com/t/setitsandboxdev427435/"
 
-token = jwt.encode(
+# Define a function to refresh the token
+def refresh_token():
+    global token
+    # Refresh the token content here
+    token = jwt.encode(
   {
     'iss': connectedAppClientId,
     'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=10),
@@ -34,10 +41,16 @@ token = jwt.encode(
     'iss': connectedAppClientId
   })
 
+# Function to demonstrate using the token
+def another_function(token):
+    return token
+
+# Schedule the token refresh every 8 minutes
+schedule.every(8).minutes.do(refresh_token)
+
 #########
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/', methods = ['GET', 'POST'])
 #@app.route('/get_string')
@@ -46,20 +59,19 @@ def get_string():
   # Call your Python function and get the string result
   text = str(request.args.get('input')) # Requests the ?input= 1
   # result = your_python_function(data)
+  # Add a loop to run the scheduled tasks
+  i=0
+  while True:
+    schedule.run_pending()
+    time.sleep(8*60+30)
+    # Return the result as JSON
+    #print('Here')
+    #return jsonify({'result': token})
+    another_function(token)
+    i+=1
+    print(i)
+    #return('<p>Hello</p>')
 
-  # Return the result as JSON
-  #print('Here')
-  return jsonify({'result': token})
-  #return('<p>Hello</p>')
-
-@app.route('/git_update', methods=['POST'])
-def git_update():
-    repo = git.Repo('./PythonAnyWhere_Tab_Auth')
-    origin = repo.remotes.origin
-    repo.create_head('main',
-                     origin.refs.main).set_tracking_branch(origin.refs.main).checkout()
-    origin.pull()
-    return '', 200
 
 #app.run(host='0.0.0.0', port = 8080)
 
